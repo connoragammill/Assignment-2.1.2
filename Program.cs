a3e0553db5a3f763ed637006324b47464aa54544
conno <conno@CONSTRAPCUM>
2024-11-11 14:37:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_2._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //                   Truck
+            //                     |
+            //                   Chevy
+            //               /     |     \
+            //              /      |      \
+            //      Mid size    Half Ton   3/4 Ton
+            //         |           |           |
+            //         |           |           |
+            //    Colorado  Silverado 1500  Silverado 2500
+        }
+    }
+}
